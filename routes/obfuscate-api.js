dad75b44f1b1b12fc905884276cac373acc1ef91
Stroketon <stroketon@elerium.cc>
2020-08-01 18:43:37 +0300

    Add actual auth which uses mongodb

--- routes/obfuscate-api.js
+++ routes/obfuscate-api.js
@@ -2,22 +2,29 @@ const menprotect = require("../menprotect");
 const express = require("express");
 const obfuscator = new menprotect();
 const router = new express.Router();
+const Utils = require("../utils");
 router.post("/", function(req, res) {
-    if (req.body.key && req.body.key == "test") {
-        obfuscator.obfuscate({
-            script: req.body.script || "do end",
-            callback: function(data) {
-                res.end(data.script);
-            },
-            log: function(info) {
-                if (info.status == 4) {
-                    res.status(400).end(info.body);
-                };
-            }
-        });
-    } else {
-        res.status(400).end("");
-    };
+    Utils.GetCollection(async function(collection, client) {
+        let Doc = await collection.findOne({
+            Key: req.body.key
+        })
+        if (Doc) {
+            obfuscator.obfuscate({
+                script: req.body.script || "do end",
+                callback: function(data) {
+                    res.end(data.script);
+                },
+                log: function(info) {
+                    if (info.status == 4) {
+                        res.status(400).end(info.body);
+                    };
+                }
+            });
+        } else {
+            res.status(400).end("");
+        };
+        client.close();
+    })
 });
 
 module.exports = {
