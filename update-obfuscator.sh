40ec32875a0c3a4d1bbc59f7cb33e207b821999e
Singularity5490 <41873715+Singularity5490@users.noreply.github.com>
2020-08-17 14:58:20 +0200

    Update update-obfuscator.sh

--- update-obfuscator.sh
+++ update-obfuscator.sh
@@ -1,3 +1,1 @@
-## This is just a update script for myself to make it easier for me to update
-sshpass -p "Lisgor43" ssh elerium -T "rm -r ./menprotect/menprotect"
-sshpass -p "Lisgor43" scp -r ./menprotect elerium:/root/menprotect
+#no 
